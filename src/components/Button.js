2e03f85e90c8f41a9a4e27a95ac53465a7a265fd
Jonathan Dodard <jonathan.dodard@gmail.com>
2018-06-05 14:15:13 +0200

    install react & create components

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => (
+    <button onClick={ ()=>console.log('test click')}>
+        🐍click me!🐍
+    </button>
+)
+
+export default Button
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('test')
+import React from 'react'
+import { render } from 'react-dom'
+import Button from 'components/Button'
+import Input from 'components/Input'
+
+render(
+    <div>
+        <h1>Hello world</h1>
+        <Button/>
+        <Input/>
+    </div>,
+    document.querySelector('#mount')
+)
